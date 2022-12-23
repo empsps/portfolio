0fcc1a57ab7d65dbfa5a2cc6ed966a9be04c122d
empsps <sophialealbarros@gmail.com>
2022-12-23 20:31:11 -0300

    feat(navbar): add shine effects to button

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -7,9 +7,9 @@ export const Navbar = () => {
       <span className='title'>JUAN BARROS</span>
       <div className='items'>
         <NavbarItem text='Teste botao' color='purple' />
-        <NavbarItem text='Teste botao' color='orange' />
         <NavbarItem text='Teste botao' color='purple' />
-        <NavbarItem text='Teste botao' color='orange' />
+        <NavbarItem text='Teste botao' color='purple' />
+        <NavbarItem shine={true} text='Contato' color='orange' />
       </div>
     </div>
   );
--- src/components/Navbar/NavbarItem.tsx
+++ src/components/Navbar/NavbarItem.tsx
@@ -3,12 +3,16 @@ import './Navbar.scss';
 type NavbarItemProps = {
   text: string;
   color: 'purple' | 'orange';
+  shine?: boolean;
 };
 
-export const NavbarItem = ({ text, color }: NavbarItemProps) => {
+export const NavbarItem = ({ text, color, shine }: NavbarItemProps) => {
   return (
-    <button className={`item item-${color}`}>
-      {text}
-    </button>
-  )
-}
+    <div className='item-container'>
+      <button className={`item item-${color}`}>
+        {text}
+        <div className='light' />
+      </button>
+    </div>
+  );
+};
